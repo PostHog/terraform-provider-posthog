9e7472005155933a800ecfe0c84f94343f8553f8
Vasco de Krijger <vasco@posthog.com>
2025-12-02 10:50:08 +0100

    chore: simplified the provider data object

--- internal/data/provider_data.go
+++ internal/data/provider_data.go
@@ -2,12 +2,9 @@ package data
 
 import (
 	"github.com/posthog/terraform-provider/internal/posthog"
-	posthogapi "github.com/posthog/terraform-provider/internal/posthog/swagger"
 )
 
 // ProviderData passes configured client to resources.
 type ProviderData struct {
-	Client        posthog.Client
-	SwaggerClient *posthogapi.APIClient
-	ProjectID     string
+	Client posthog.PosthogClient
 }
